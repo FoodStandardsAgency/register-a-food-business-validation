7c38f6728501ce2a6f7d0d0d6626d7d1af52433c
Django Shelton <djangoshelton@gmail.com>
2018-06-21 15:22:20 +0100

    validateEstablishmentFirstLine function

---
+++ validationFunctions/validateEstablishmentFirstLine.js
@@ -0,0 +1,11 @@
+const { isAscii, isEmpty, trim } = require("validator");
+
+const validateEstablishmentFirstLine = firstLine => {
+  if (typeof firstLine === "string") {
+    if (isEmpty(trim(firstLine))) return false;
+    return isAscii(firstLine) ? true : false;
+  }
+  return false;
+};
+
+module.exports = validateEstablishmentFirstLine;
