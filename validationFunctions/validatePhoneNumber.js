9c2bd362411f7e2a37ce808192d5172c23428d4d
Django Shelton <djangoshelton@gmail.com>
2018-06-21 15:59:47 +0100

    linting fixes

--- validationFunctions/validatePhoneNumber.js
+++ validationFunctions/validatePhoneNumber.js
@@ -1,14 +1,14 @@
-const { isNumeric, isEmpty } = require("validator");
+const { isNumeric } = require("validator");
 
 const validatePhoneNumber = phoneNumber => {
-    if (typeof phoneNumber === "string") {
-        phoneNumber = phoneNumber.split(' ').join('');
-        if (phoneNumber.startsWith("+")) {
-            phoneNumber = phoneNumber.substring(1);
-        }
-        return isNumeric(phoneNumber) ? true : false;
+  if (typeof phoneNumber === "string") {
+    phoneNumber = phoneNumber.split(" ").join("");
+    if (phoneNumber.startsWith("+")) {
+      phoneNumber = phoneNumber.substring(1);
     }
-    return false;
+    return isNumeric(phoneNumber) ? true : false;
+  }
+  return false;
 };
 
 module.exports = validatePhoneNumber;
--- validationFunctions/validatePhoneNumberOptional.js
+++ validationFunctions/validatePhoneNumberOptional.js
@@ -1,13 +1,14 @@
-const { isNumeric, isEmpty } = require("validator");
+const { isEmpty } = require("validator");
 const validatePhoneNumber = require("./validatePhoneNumber");
 
-
 const validatePhoneNumberOptional = phoneNumber => {
-    if (typeof phoneNumber === "string") {
-        if (isEmpty(phoneNumber)) return true;
-        return validatePhoneNumber(phoneNumber);
+  if (typeof phoneNumber === "string") {
+    if (isEmpty(phoneNumber)) {
+      return true;
     }
-    return false;
+    return validatePhoneNumber(phoneNumber);
+  }
+  return false;
 };
 
 module.exports = validatePhoneNumberOptional;
