0550d4ad9920d1a1ba9410af45f98a651d3c09f4
Django Shelton <djangoshelton@gmail.com>
2018-06-21 15:30:00 +0100

    validateTown function

---
+++ validationFunctions/validateTown.js
@@ -0,0 +1,12 @@
+const { isAscii, isEmpty, trim } = require("validator");
+
+const validateTown = town => {
+  if (typeof town === "string") {
+    if (isEmpty(town)) return true;
+    if (isEmpty(trim(town))) return false;
+    return isAscii(town) ? true : false;
+  }
+  return false;
+};
+
+module.exports = validateTown;
