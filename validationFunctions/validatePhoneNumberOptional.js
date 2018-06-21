8a1babfc3058fd1e839e1cb87b80f7b544deb820
A. P. Malde <anisha.malde@ibm.com>
2018-06-21 15:50:39 +0100

    validate phone number optional

---
+++ validationFunctions/validatePhoneNumberOptional.js
@@ -0,0 +1,13 @@
+const { isNumeric, isEmpty } = require("validator");
+const validatePhoneNumber = require("./validatePhoneNumber");
+
+
+const validatePhoneNumberOptional = phoneNumber => {
+    if (typeof phoneNumber === "string") {
+        if (isEmpty(phoneNumber)) return true;
+        return validatePhoneNumber(phoneNumber);
+    }
+    return false;
+};
+
+module.exports = validatePhoneNumberOptional;
