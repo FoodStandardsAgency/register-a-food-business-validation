7c5007db8e4673011a7df25f9612b80b7e10dc70
Django Shelton <djangoshelton@gmail.com>
2018-06-21 16:09:50 +0100

    export all functions

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const validateDeclaration = require('./validationFunctions/validateDeclaration');
+const validateDeclaration = require("./validationFunctions/validateDeclaration");
+const validateEmail = require("./validationFunctions/validateEmail");
+const validateEstablishmentFirstLine = require("./validationFunctions/validateEstablishmentFirstLine");
+const validateEstablishmentTradingName = require("./validationFunctions/validateEstablishmentTradingName");
+const validateName = require("./validationFunctions/validateName");
+const validatePhoneNumber = require("./validationFunctions/validatePhoneNumber");
+const validatePhoneNumberOptional = require("./validationFunctions/validatePhoneNumberOptional");
+const validatePostcode = require("./validationFunctions/validatePostcode");
+const validateStreet = require("./validationFunctions/validateStreet");
+const validateTown = require("./validationFunctions/validateTown");
 
 module.exports = {
-  validateDeclaration
-}
+  validateDeclaration,
+  validateEmail,
+  validateEstablishmentFirstLine,
+  validateEstablishmentTradingName,
+  validateName,
+  validatePhoneNumber,
+  validatePhoneNumberOptional,
+  validatePostcode,
+  validateStreet,
+  validateTown
+};
