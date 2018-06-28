fb245cb3644da952c701dc289aaaf033905953d7
A. P. Malde <anisha.malde@ibm.com>
2018-06-28 17:15:47 +0100

    Not letting more than 1 dash pass

--- validationFunctions/validateCharityNumber.js
+++ validationFunctions/validateCharityNumber.js
@@ -2,15 +2,18 @@ const { isEmpty, trim, isAlphanumeric, blacklist } = require("validator");
 
 const validateCharityNumber = charityNumber => {
     if (typeof charityNumber === "string") {
-        const charityNumberNoDash = blacklist(charityNumber, '-');
-        const noWhiteSpaceCharityNumber = trim(charityNumberNoDash);
+        const noWhiteSpaceCharityNumber = trim(charityNumber);
         if (isEmpty(noWhiteSpaceCharityNumber)) {
             return false;
         }
         if (noWhiteSpaceCharityNumber.length > 8 || noWhiteSpaceCharityNumber.length < 6) {
             return false;
         }
-        if (isAlphanumeric(noWhiteSpaceCharityNumber, "en-GB") === false) {
+        if ((noWhiteSpaceCharityNumber.match(new RegExp("-", "g")) || []).length > 1) {
+            return false;
+        }
+        const charityNumberNoDash = blacklist(noWhiteSpaceCharityNumber, '-');
+        if (isAlphanumeric(charityNumberNoDash, "en-GB") === false) {
             return false;
         }
         return true;
--- validationFunctions/validateCharityNumber.test.js
+++ validationFunctions/validateCharityNumber.test.js
@@ -50,9 +50,22 @@ describe("Function: validateCharityNumber", () => {
         });
     });
 
+    it("should return false if charity number contains more than one - ", () => {
+        // Arrange
+        const charityNumbers = ["123--678", "--------", "------"];
+
+        charityNumbers.forEach(charityNumber => {
+            // Act
+            const valid = validateCharityNumber(charityNumber);
+
+            // Assert
+            expect(valid).toBe(false);
+        });
+    });
+
     it("should return true if companies house number is 6-8 characters, Alphanumeric and may contain dashes", () => {
         // Arrange
-        const charityNumbers = ["12345678", "123456", "12345-78", "--123456"];
+        const charityNumbers = ["12345678", "123456", "12345-78", "-123456", "-12345"];
 
         charityNumbers.forEach(charityNumber => {
             // Act
