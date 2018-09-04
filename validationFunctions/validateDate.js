e63d32b8bc1550b4e67a9aa00c18e3517f1694bd
A. P. Malde <anisha.malde@ibm.com>
2018-09-04 14:44:19 +0100

    Change from review

--- validationFunctions/validateDate.js
+++ validationFunctions/validateDate.js
@@ -2,7 +2,7 @@ const { isISO8601 } = require("validator");
 
 const validateDate = date => {
   if (typeof date === "string") {
-    dateArray = date.split("-");
+    const dateArray = date.split("-");
     if (dateArray[1] < 10 && dateArray[1].indexOf("0") === -1) {
       dateArray[1] = `0${dateArray[1]}`;
     }
