2d3654a5cd87b98ba461d69eca1f532fc17e9d7a
Michal Rak <michal.rak@civica.co.uk>
2019-05-01 11:47:50 +0000

    update attribute reference and check for partners length

--- validationFunctions/validatePartnerPrimaryContact.js
+++ validationFunctions/validatePartnerPrimaryContact.js
@@ -1,10 +1,12 @@
 const validatePartnerPrimaryContact = partners => {
   if (Array.isArray(partners)) {
-    const mainPartnershipContacts = partners.filter(
-      partner => partner.is_primary_contact === true
-    );
-    if (mainPartnershipContacts.length === 1) {
-      return true;
+    if (partners.length >= 2 && partners.length <= 5) {
+      const mainPartnershipContacts = partners.filter(
+        partner => partner.partner_is_primary_contact === true
+      );
+      if (mainPartnershipContacts.length === 1) {
+        return true;
+      }
     }
   }
   return false;
