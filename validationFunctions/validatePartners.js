e67d39b06fd5406b2e5ca4dc6a5245efdd7ee128
David Kasali <david.kasali@civica.co.uk>
2021-01-15 16:28:49 +0000

    merge of functions

--- validationFunctions/validatePartners.js
+++ validationFunctions/validatePartners.js
@@ -4,17 +4,24 @@
  */
 
 /**
- * Runs custom validation on the partners array. It will be true if the supplied argument is an array having between 2 and 5 items.
+ * Runs custom validation on the partners array. It will be true if the supplied argument is an array having between 2 and 5 items with no duplicate partner values.
  *
  * @param {Object[]} partners The list of partners supplied by the user
  *
- * @returns {boolean} It will return true if the argument is an array with a minimum of 2 and a maximum of 5 items, false otherwise.
+ * @returns {boolean} It will return true if the argument is an array with a minimum of 2 and a maximum of 5 items with no duplicate partner values, false otherwise.
  */
 
-const validatePartners = partners => {
+const validatePartners = (partners) => {
   if (Array.isArray(partners)) {
-    if (partners.length >= 2 && partners.length <= 5) {
+    var uniquePartners = new Set(partners);
+    if (
+      partners.length >= 2 &&
+      partners.length <= 5 &&
+      uniquePartners.size === partners.length
+    ) {
       return true;
+    } else {
+      return false;
     }
   }
   return false;
--- validationFunctions/validatePartners.test.js
+++ validationFunctions/validatePartners.test.js
@@ -5,7 +5,7 @@ describe("Function: validatePartners", () => {
     // Arrange
     const badPartners = ["", {}, null, undefined];
     // Act
-    badPartners.forEach(partner => {
+    badPartners.forEach((partner) => {
       // Assert
       const valid = validatePartners(partner);
       expect(valid).toBe(false);
@@ -15,24 +15,28 @@ describe("Function: validatePartners", () => {
   it("Should return false if array length is above 5", () => {
     //Arrange
     const badPartnersLength = [
-      { name: "", is_primary_contact: true },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false }
+      "jon",
+      "Mike",
+      "jo",
+      "dan",
+      "wes",
+      "exa",
     ];
     //Act
-    const valid = validatePartners(badPartnersLength);
+    const valid = validatePartners(
+      badPartnersLength
+    );
     //Assert
     expect(valid).toBe(false);
   });
 
   it("Should return false if array length is below 2", () => {
     //Arrange
-    const badPartnersLength = [{ name: "", is_primary_contact: true }];
+    const badPartnersLength = ["was"];
     //Act
-    const valid = validatePartners(badPartnersLength);
+    const valid = validatePartners(
+      badPartnersLength
+    );
     //Assert
     expect(valid).toBe(false);
   });
@@ -40,25 +44,26 @@ describe("Function: validatePartners", () => {
   it("Should return true when the array length is between 2-5", () => {
     //Arrange
     const goodPartnersLength = [
-      { name: "", is_primary_contact: true },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false }
+      "jon",
+      "Mike",
+      "jo",
+      "dan",
     ];
     //Act
-    const valid = validatePartners(goodPartnersLength);
+    const valid = validatePartners(
+      goodPartnersLength
+    );
     //Assert
     expect(valid).toBe(true);
   });
 
   it("Should return true when the array length is exactly 2", () => {
     //Arrange
-    const goodPartnersLength = [
-      { name: "", is_primary_contact: true },
-      { name: "", is_primary_contact: false }
-    ];
+    const goodPartnersLength = ["jon", "exa"];
     //Act
-    const valid = validatePartners(goodPartnersLength);
+    const valid = validatePartners(
+      goodPartnersLength
+    );
     //Assert
     expect(valid).toBe(true);
   });
@@ -66,14 +71,62 @@ describe("Function: validatePartners", () => {
   it("Should return true when the array length is exactly 5", () => {
     //Arrange
     const goodPartnersLength = [
-      { name: "", is_primary_contact: true },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false },
-      { name: "", is_primary_contact: false }
+      "jon",
+      "Mike",
+      "jo",
+      "dan",
+      "exa",
     ];
     //Act
-    const valid = validatePartners(goodPartnersLength);
+    const valid = validatePartners(
+      goodPartnersLength
+    );
+    //Assert
+    expect(valid).toBe(true);
+  });
+});
+
+describe("Function: validatePartners", () => {
+  it("Should return false if consecutive values in array are duplicated", () => {
+    //Arrange
+    const duplicatesPartnersArray = [
+      "John Barnes",
+      "John Barnes",
+      "Matt",
+    ];
+    //Act
+    const valid = validatePartners(
+      duplicatesPartnersArray
+    );
+    //Assert
+    expect(valid).toBe(false);
+  });
+
+  it("Should return false if any value in array is duplicated", () => {
+    //Arrange
+    const duplicatesPartnersArray = [
+      "John Barnes",
+      "Matt",
+      "John Barnes",
+    ];
+    //Act
+    const valid = validatePartners(
+      duplicatesPartnersArray
+    );
+    //Assert
+    expect(valid).toBe(false);
+  });
+
+  it("Should return true if array has no duplicate values", () => {
+    //Arrange
+    const validPartnersArray = [
+      "Matt",
+      "John Barnes",
+    ];
+    //Act
+    const valid = validatePartners(
+      validPartnersArray
+    );
     //Assert
     expect(valid).toBe(true);
   });
