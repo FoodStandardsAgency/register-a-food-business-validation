25fcd5ad1462f19f59320cc16241623070e3ef3a
David Kasali <david.kasali@civica.co.uk>
2021-03-17 11:20:55 +0000

    add web address validation and test

---
+++ validationFunctions/validateWebAddress.js
@@ -0,0 +1,25 @@
+/**
+ * Function for validating the web address optional field
+ * @module functions/validateWebAddress
+ */
+const { isURL, trim } = require("validator");
+
+/**
+ * Runs custom validation on the string web address. It will be true if the input is less than 256 characters. It will be true if the input is a valid web address compliant with the validator npm package. 
+ *
+ * @param {string} webAddress The text string of the web address the user supplies
+ *
+ * @returns {boolean} It will return true if the string is valid and false if it is not valid
+ */
+
+const validateWebAddress = webAddress => {
+  if (typeof webAddress === "string" ) {
+    if (webAddress.length <= 255) {
+      const noWhiteSpaceWebAddress = trim(webAddress);
+      return isURL(noWhiteSpaceWebAddress);
+    }
+    return false;
+  }
+};
+
+module.exports = validateWebAddress;
