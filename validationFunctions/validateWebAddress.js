ce9cd2d505ac5341785bff174c3e95af5772f4b9
David Kasali <david.kasali@civica.co.uk>
2022-01-12 14:35:38 +0000

    web address validation

---
+++ validationFunctions/validateWebAddress.js
@@ -0,0 +1,30 @@
+/**
+ * Function for validating the web address optional field
+ * @module functions/validateWebAddress
+ */
+const { isURL, trim, isEmpty } = require("validator");
+
+/**
+ * Runs custom validation on the string web address. It will be true if the input is less than 256 characters. It will be true if the input is a valid web address compliant with the validator npm package. Empty string will return true as the field is optional.
+ *
+ * @param {string} webAddress The text string of the webAddress the user supplies
+ *
+ * @returns {boolean} It will return true if the string is valid and false if it is not valid
+ */
+
+const validateWebAddress = webAddress => {
+  if (typeof webAddress === "string") {
+    if (isEmpty(webAddress)) {
+      return true;
+    }
+    if (webAddress.length <= 255) {
+      const noWhiteSpacewebAddress = trim(webAddress);
+      return isURL(noWhiteSpacewebAddress);
+    }
+    return false;
+  }
+  return false;
+};
+
+module.exports = validateWebAddress;
+
---
+++ validationFunctions/validateWebAddress.test.js
@@ -0,0 +1,70 @@
+const validateWebAddress = require("./validateWebAddress.js");
+
+describe("Function: validate web address", () => {
+  it("Should return false when type is not string", () => {
+    // Arrange
+    const badNumber = [[], {}, null, undefined];
+    // Act
+    badNumber.forEach(number => {
+      // Assert
+      const valid = validateWebAddress(number);
+      expect(valid).toBe(false);
+    });
+  });
+
+  it("Should return true when the input is non-empty", () => {
+    //Arrange
+    const goodaddress = ["uygffd.com", "kjhfds.co.uk", "njlhbhjgcfxdf.net"];
+    //Act
+    goodaddress.forEach(number => {
+      //Assert
+      const valid = validateWebAddress(number);
+      expect(valid).toBe(true);
+    });
+  });
+
+  it("Should return true when input is empty", () => {
+    // Arrange
+    const webAddress = "";
+    // Act
+    const valid = validateWebAddress(webAddress);
+    //Assert
+    expect(valid).toBe(true);
+  });
+
+  it("Should return true when input is 255 characters or less with or without trailing spaces", () => {
+    // Arrange
+    const goodStrings = [
+      " Iamagoodstring.com",
+      "BestString.net ",
+      "IamagoodstringIamagoodstringIamagoodstringIamagoodstring.co.uk"
+    ];
+
+    // Act
+    goodStrings.forEach(string => {
+      // Assert
+      const valid = validateWebAddress(string);
+      expect(valid).toBe(true);
+    });
+  });
+
+  it("Should return false when input is over 255 characters", () => {
+    // Arrange
+    const badStrings = [
+      "IamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodIamagoodstringIamagoodstringIamagoodstringIamagoodstring.co.uk."
+    ];
+
+    // Act
+    badStrings.forEach(string => {
+      // Assert
+      const valid = validateWebAddress(string);
+      expect(valid).toBe(false);
+    });
+  });
+});
+
+
+
+
+
+
