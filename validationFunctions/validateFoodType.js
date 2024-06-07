19b66045b8595146c1ccb358335b9ec145b63e99
Joe-Cun-Civica <joseph.cunningham@civica.co.uk>
2024-06-07 14:49:38 +0100

    updated foodtype

--- validationFunctions/validateFoodType.js
+++ validationFunctions/validateFoodType.js
@@ -11,27 +11,27 @@ const { foodTypeEnum } = require("../enums/foodTypeEnum");
  * - 'NONE' and 'DONT_KNOW' can only be selected exclusively.
  * An empty array will return false as the field is mandatory.
  *
- * @param {Array<string>} foodType The array of strings representing the selected food type activities
+ * @param {Array<string>} foodTypes The array of strings representing the selected food type activities
  *
  * @returns {boolean} It will return true if the input is valid and false if it is not valid
  */
-const validateFoodType = (foodType) => {
-  if (!Array.isArray(foodType) || foodType.length === 0) {
+const validateFoodType = (foodTypes) => {
+  if (!Array.isArray(foodTypes) || foodTypes.length === 0) {
     return false;
   }
 
   // Check if 'NONE' or 'DONT_KNOW' are selected
-  const hasNone = foodType.includes(foodTypeEnum.NONE.key);
-  const hasDontKnow = foodType.includes(foodTypeEnum.DONT_KNOW.key);
+  const hasNone = foodTypes.includes(foodTypeEnum.NONE.key);
+  const hasDontKnow = foodTypes.includes(foodTypeEnum.DONT_KNOW.key);
 
   // If 'NONE' or 'DONT_KNOW' are selected, they must be the only selection
-  if ((hasNone || hasDontKnow) && foodType.length > 1) {
+  if ((hasNone || hasDontKnow) && foodTypes.length > 1) {
     return false;
   }
 
   // Validate that each selected value is part of the foodTypeEnum
-  for (let activity of foodType) {
-    if (!foodTypeEnum[activity]) {
+  for (let foodType of foodTypes) {
+    if (!foodTypeEnum[foodType]) {
       return false;
     }
   }
