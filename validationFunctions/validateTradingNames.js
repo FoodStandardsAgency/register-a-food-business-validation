3de55e9d1b91287931245f8c2edbe12e5b31d94d
Joe-Cun-Civica <joseph.cunningham@civica.co.uk>
2025-01-29 10:57:39 +0000

    Creation of trading names validation

---
+++ validationFunctions/validateTradingNames.js
@@ -0,0 +1,28 @@
+/**
+ * Function for validating the trading names mandatory field
+ * @module functions/validateTradingNames
+ */
+
+/**
+ * Runs custom validation on the partners array. It will be true if the supplied argument is an array having between 2 and 5 items with no duplicate partner values.
+ *
+ * @param {Object[]} tradingNames The list of partners supplied by the user
+ *
+ * @returns {boolean} It will return true if the argument is an array maximum of 10 items with no duplicate partner values, false otherwise.
+ */
+
+const validateTradingNames = (tradingNames) => {
+  if (Array.isArray(tradingNames)) {
+    var uniqueTradingNames = new Set(tradingNames);
+    if (
+      tradingNames.length >= 1 &&
+      tradingNames.length <= 10 &&
+      uniqueTradingNames.size === tradingNames.length
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = validateTradingNames;
