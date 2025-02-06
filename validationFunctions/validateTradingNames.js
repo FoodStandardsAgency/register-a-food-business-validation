1d9e7dde8399d965d6e25229221326afce2f4b24
Ernestas Leliuga <109216370+ernestas-leliuga@users.noreply.github.com>
2025-02-06 14:47:33 +0000

    fix validateTradingNames.js

--- validationFunctions/validateTradingNames.js
+++ validationFunctions/validateTradingNames.js
@@ -4,18 +4,17 @@
  */
 
 /**
- *  Runs custom validation on the partners array. It will be true if the supplied argument is an array having between 2 and 5 items with no duplicate partner values.
+ *  Runs custom validation on the tradingNames array. It will be true if the supplied argument is an array having between 0 and 10 items with no duplicate trading name values.
  *
- * @param {Object[]} tradingNames The list of partners supplied by the user
+ * @param {Object[]} tradingNames The list of additional trading names supplied by the user
  *
- * @returns {boolean} It will return true if the argument is an array maximum of 10 items with no duplicate partner values, false otherwise.
+ * @returns {boolean} It will return true if the argument is an array maximum of 10 items with no duplicate trading names values, false otherwise.
  */
 
 const validateTradingNames = (tradingNames) => {
   if (Array.isArray(tradingNames)) {
     var uniqueTradingNames = new Set(tradingNames);
     if (
-      tradingNames.length >= 1 &&
       tradingNames.length <= 10 &&
       uniqueTradingNames.size === tradingNames.length
     ) {
